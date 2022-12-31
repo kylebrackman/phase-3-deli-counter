f22d7e8322cb12aeee9248c56510ff5d07779a33
Kyle Brackman <kyle.a.brackman@gmail.com>
2022-12-31 12:00:29 -0700

    commit tree 64f1b4d127c34d596562500a2f3cafae43bfc983

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,9 @@
 # Write your code here.
+def line deli
+    if deli.empty?
+        puts "The line is currently empty."
+    else 
+        puts deli.each do |person|
+        puts person
+    end
+end
